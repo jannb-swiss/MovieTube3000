c24de4b99b7b5870a694848b4622f33fd382bf2f
jannb-swiss <jan.nussberger@outlook.com>
2021-06-11 14:36:57 +0200

    push movie-genere

---
+++ src/app/movie-genere/movie-genere.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-genere',
+  templateUrl: './movie-genere.component.html',
+  styleUrls: ['./movie-genere.component.scss']
+})
+export class MovieGenereComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
